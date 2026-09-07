e88c3112fca2de1516e583549b06e080eab5bfdf
Renan Brandão <renan.br31@gmail.com>
2026-09-07 20:23:13 -0300

    fazendo ex016

---
+++ aula014ex/ex016/script.js
@@ -0,0 +1,28 @@
+let bt = document.getElementById('bt')
+let inicio = document.getElementById('inicio')
+let fim = document.getElementById('fim')
+let passo = document.getElementById('passo')
+bt.addEventListener("click", contar)
+let res = document.getElementById('res')
+
+function contar() {
+    if (inicio.value.length === 0 || fim.value.length === 0 || passo.value.length === 0) {
+        alert('Digite um valor válido.')
+    }   else {
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+    
+        if (p <= 0 ) {
+            alert('Passo inválido! Considerando passo como 1.')
+            p = 1
+        }
+        res.innerHTML = ''
+        let numeros = []
+        while(i <= f) { 
+        numeros.push(i)
+        i += p 
+        }
+        res.innerHTML = numeros.join(` ->  `)
+    }
+}
